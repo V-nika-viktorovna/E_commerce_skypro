257f6478ca26d597fead7f80e489d5bbeabfb9b9
Veronika Chernikova <fotonikanvr@yandex.ru>
2025-03-03 21:44:17 +0300

    Домашняя работа 10.03.25_1 Исправления

--- tests/test_Category.py
+++ tests/test_Category.py
@@ -1,4 +1,9 @@
+from src.Category import Category
+
+
 def test_Category_try(categoryes_fix):
     assert categoryes_fix.name == "Смартфоны"
     assert categoryes_fix.description == "Смартфоны, как средство коммуникации"
     assert categoryes_fix.products == ["Fly GS Ultra", "Iphone 15"]
+    assert Category.category_count == 1
+    assert Category.product_count == 2
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -7,27 +7,27 @@ from src.utils import get_data_object_class
 @patch('json.load')
 def test_get_data_object_class_try(mock_get):
     mock_get.return_value = [{'name': 'Смартфончики :)))',
-                             'description': 'Смартфоны, как средство не только коммуникации, \
-                             но и получение дополнительных функций для удобства жизни',
-                             'products': [
-                                 {'name': 'Samsung Galaxy C23 Ultra',
-                                  'description': '256GB, Серый цвет, 200MP камера',
-                                  'price': 180000.0, 'quantity': 5},
-                                 {'name': 'Iphone 15',
-                                  'description': '512GB, Gray space',
-                                  'price': 210000.0, 'quantity': 8},
-                                 {'name': 'Xiaomi Redmi Note 11',
-                                  'description': '1024GB, Синий',
-                                  'price': 31000.0, 'quantity': 14}]},
-                            {'name': 'Телевизоры - обзоры',
-                             'description': 'Современный телевизор, который позволяет наслаждаться просмотром, \
-                             станет вашим другом и помощником',
-                             'products': [
-                                 {'name': '55" QLED 4K',
-                                  'description': 'Фоновая подсветка',
-                                  'price': 123000.0,
-                                  'quantity': 7}]}
-                            ]
+                              'description': 'Смартфоны, как средство не только коммуникации, \
+                              но и получение дополнительных функций для удобства жизни',
+                              'products': [
+                                  {'name': 'Samsung Galaxy C23 Ultra',
+                                   'description': '256GB, Серый цвет, 200MP камера',
+                                   'price': 180000.0, 'quantity': 5},
+                                  {'name': 'Iphone 15',
+                                   'description': '512GB, Gray space',
+                                   'price': 210000.0, 'quantity': 8},
+                                  {'name': 'Xiaomi Redmi Note 11',
+                                   'description': '1024GB, Синий',
+                                   'price': 31000.0, 'quantity': 14}]},
+                             {'name': 'Телевизоры - обзоры',
+                              'description': 'Современный телевизор, который позволяет наслаждаться просмотром, \
+                              станет вашим другом и помощником',
+                              'products': [
+                                  {'name': '55" QLED 4K',
+                                   'description': 'Фоновая подсветка',
+                                   'price': 123000.0,
+                                   'quantity': 7}]}
+                             ]
     CURRENT_DIR = os.path.dirname(__file__)
     DATA_DIR = os.path.join(CURRENT_DIR, '..', 'data')
     FILE_DIR = os.path.join(DATA_DIR, 'products_test.json')
